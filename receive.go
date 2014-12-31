d5609ea638039883f2695b1530394c3b4926be9f
skarflacka-macbook <skarflacka232@gmail.com>
2014-12-31 15:38:29 +0100

    Add move to images and fix redirect when uploading

--- receive.go
+++ receive.go
@@ -5,29 +5,44 @@ import (
 	"net/http" 
 	"log" 
 	"io/ioutil" 
+	"time"
+	"os"
 ) 
 
 // hello world, the web server 
 func Server(w http.ResponseWriter, req *http.Request) { 
+
 	file, handler, err := req.FormFile("file") 
 	if err != nil { 
-			fmt.Println(err) 
+		fmt.Println(err) 
 	} 
 
 	data, err := ioutil.ReadAll(file) 
 	if err != nil { 
-			fmt.Println(err) 
-	} 
+		fmt.Println(err) 
+	}
 
 	err = ioutil.WriteFile(handler.Filename, data, 0777) 
 	if err != nil { 
-			fmt.Println(err) 
+		fmt.Println(err) 
 	}
 
+	var s string
+
+	s = handler.Filename
+
+	fmt.Println("Upload: " + s)
+
+	eerrr := os.Rename("/Users/tony/uploader/" + s, "/Users/tony/uploader/images/" + s)
+	if eerrr != nil {
+		fmt.Println(err)
+	}
 	
 } 
 
 func main() { 
+	fmt.Println(time.Now())
+
 	http.HandleFunc("/upload", Server) 
 	
 	err := http.ListenAndServe(":8080", nil) 
